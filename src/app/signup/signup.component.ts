262771dcd7ee2e9e18fff22361d1d2f758d9fe5c
Gabriel Fernandes Silva <gabezk>
2024-06-23 14:34:13 -0300

    add PrimeNG component library

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputIconModule } from 'primeng/inputicon';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    IconFieldModule,
+    InputIconModule,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
